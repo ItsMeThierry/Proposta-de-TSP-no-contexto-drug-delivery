9075106a7dea320381dc16a8f4141790576eefed
ItsMeThierry <thierrytorquato@gmail.com>
2023-10-21 14:26:05 -0300

    21_10.0.0

--- tsp.py
+++ tsp.py
@@ -1,30 +1,44 @@
 from concorde.problem import Problem
 from concorde.concorde import Concorde
-import math
+from concorde.tsp import TSPSolver
+import tsplib95
 
 ##Valeu Guilherme
-matriz = []
+def resolve():
+    matriz = []
 
-with open('matriz_1.txt', 'r') as f:
-    for linha in f:
+    with open('output_matrix.txt', 'r') as f:
+        for linha in f:
 
-        valores_tmp = linha.strip().split(' ')
-        valores = []
+            valores_tmp = linha.strip().split(' ')
+            valores = []
 
-        for valor in valores_tmp:
-            if valor != '\n':
-                valores.append(float(valor))
+            for valor in valores_tmp:
+                if valor != '\n':
+                    valores.append(float(valor))
             
-        matriz.append(valores) 
+            matriz.append(valores) 
 
-for i in range(len(matriz)):
-    for j in range(i, len(matriz)):
-        media = (matriz[i][j] + matriz[j][i])/2.0
-        matriz[i][j] = '{:.1f}'.format(media)
-        matriz[j][i] = '{:.1f}'.format(media)
+    for i in range(len(matriz)):
+        for j in range(i, len(matriz)):
+            media = (matriz[i][j] + matriz[j][i])/2.0
+            matriz[i][j] = '{:.1f}'.format(media)
+            matriz[j][i] = '{:.1f}'.format(media)
+        
+    problem = tsplib95.models.StandardProblem()
 
+    problem.name = "Teste"
+    problem.type = "TSP"
+    problem.dimension = len(matriz)
+    problem.edge_weight_type = "EXPLICIT"
+    problem.edge_weight_format = "FULL_MATRIX"
+    problem.node_coord_type = "NO_COORDS"
+    problem.display_data_type = "NO_DISPLAY"
+    problem.edge_weights = matriz
 
+    problem.save(f'teste.tsp')
+    solver = TSPSolver.from_tspfile(f'teste.tsp')
+    solution = solver.solve()
 
-problem = Problem.from_matrix(matriz)
-solution = Concorde().solve(problem)
-print(solution.tour)
+    f_out = open('output.txt', 'w')
+    f_out.write(solution.tour)
